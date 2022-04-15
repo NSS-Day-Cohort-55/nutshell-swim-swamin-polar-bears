d899b60edb428defdcc33b05fad4af1598de0c35
larsencs <quietus77@gmail.com>
2022-04-15 13:54:59 -0500

    merging main

--- src/components/modules/WeatherManager.js
+++ src/components/modules/WeatherManager.js
@@ -7,3 +7,11 @@ export const getLocalWeather = () =>{
     return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${settings.nashville.lat}&lon=${settings.nashville.long}&timezone=America/Chicago&units=imperial&appid=${settings.weatherKey}`)
         .then(response => response.json())
 }
+
+const getGeoCode = (city, state, apikey) =>{
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},+1&limit=5&appid=${apikey}`)
+        .then(response => response.json())
+        .then(things => {
+            return things
+        })
+}
