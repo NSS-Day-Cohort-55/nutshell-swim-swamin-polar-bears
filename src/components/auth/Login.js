bdaf9c7994dbeba6ffe4b01f35cbdb9d1ca4b7f7
k-sarito <coryclark95@gmail.com>
2022-04-18 09:49:23 -0500

    PARTICLESgit add --all!

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,7 @@
 import React, { useRef, useState } from "react";
+import Particles from "react-tsparticles";
+import { loadFull} from "tsparticles"
+
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
@@ -7,6 +10,16 @@ export const Login = ({setAuthUser}) => {
 	const [existDialog, setExistDialog] = useState(false);
 
 	const navigate = useNavigate();
+
+	const particlesInit = async (main) => {
+		console.log(main);
+
+		await loadFull(main);
+	};
+
+	const particlesLoaded = (container) => {
+		console.log(container)
+	}
 
 	const handleInputChange = (event) => {
 		const newUser = { ...loginUser };
@@ -71,6 +84,86 @@ export const Login = ({setAuthUser}) => {
 			<section className="link--register">
 				<Link to="/register">Register for an account</Link>
 			</section>
+			<div>
+
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				loaded={particlesLoaded}
+				options={{
+					background: {
+					// color: {
+					// 	value: "#0d47a1",
+					// },
+					},
+					fpsLimit: 120,
+					interactivity: {
+					events: {
+						onClick: {
+						enable: true,
+						mode: "push",
+						},
+						onHover: {
+						enable: true,
+						mode: "repulse",
+						},
+						resize: true,
+					},
+					modes: {
+						push: {
+						quantity: 4,
+						},
+						repulse: {
+						distance: 200,
+						duration: 0.4,
+						},
+					},
+					},
+					particles: {
+					color: {
+						value: "#ffffff",
+					},
+					links: {
+						color: "#ffffff",
+						distance: 150,
+						enable: true,
+						opacity: 0.5,
+						width: 1,
+					},
+					collisions: {
+						enable: true,
+					},
+					move: {
+						direction: "none",
+						enable: true,
+						outModes: {
+						default: "bounce",
+						},
+						random: false,
+						speed: 1,
+						straight: false,
+					},
+					number: {
+						density: {
+						enable: true,
+						area: 800,
+						},
+						value: 80,
+					},
+					opacity: {
+						value: 0.5,
+					},
+					shape: {
+						type: "polygon",
+					},
+					size: {
+						value: { min: 1, max: 1 },
+					},
+					},
+					detectRetina: true,
+				}}
+    		/>
+			</div>
 		</main>
 	);
 };
