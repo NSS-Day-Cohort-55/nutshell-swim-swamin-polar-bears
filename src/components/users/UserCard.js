0010d801e1d92180c014d04b7bf8561fb65f794a
larsencs <quietus77@gmail.com>
2022-04-19 09:42:02 -0500

    search bar exists

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -5,17 +5,21 @@ import { Link } from "react-router-dom";
 export const UserCard = ({ user, handleAddFriend }) => {
   return (
     <>
-      <div className="card">
-        <div className="card-content">
-          <h3>
-            Name: <span className="card-username">{user.name}</span>
-          </h3>
-          <p>Email: {user.email}</p>
-          <button type="button" onClick={() => handleAddFriend(user.id)}>
-            Add As Friend
-          </button>
-        </div>
-      </div>
+    <label htmlFor="search_bar">Find friends</label>
+    <input type="text" id="search_bar"/>
+    <button type="button" id="search_btn">Search</button>
     </>
   );
 };
+
+{/* <div className="card">
+  <div className="card-content">
+    <h3>
+      Name: <span className="card-username">{user.name}</span>
+    </h3>
+    <p>Email: {user.email}</p>
+    <button type="button" onClick={() => handleAddFriend(user.id)}>
+      Add As Friend
+    </button>
+  </div>
+</div> */}
